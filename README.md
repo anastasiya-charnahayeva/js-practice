bfae138e95c7e77ba0192d6cd5498682d174fe1a
anastasiya-charnahayeva <anastasiya.charnahayeva@innowise.com>
2024-04-16 11:25:16 +0200

    base settings for the app

---
+++ README.md
@@ -0,0 +1,12 @@
+# Data visualization dashboard
+> This is the project that provides the ability to get  and visualizate the data of JSON Api
+
+## How to run the app
+> To run the app you need to use command ```npm run serve ```
+
+
+# Дэшборд для отображения данных
+> Это проект, который предоставляет возможность получать и отображать данные с JSON апи
+
+## Как запустить приложение
+> Для запуска приложения вам необходимо использовать команду ```npm run serve ```
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './index.html'
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
